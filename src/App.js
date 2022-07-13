b42a157667bb5b373c46e852ba08aeb0cd48a1a5
Ashish Katiyar <katiyarashish24@gmail.com>
2022-07-14 00:03:32 +0530

    basic routing and some pages done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navbar from "./Components/Navbar/Navbar";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import Home from "./Pages/Home/Home";
+import Label from "./Pages/Label/Label";
+import Archive from "./Pages/Archive/Archive";
+import Trash from "./Pages/Trash/Trash";
 import "./App.css";
-import logo from "./logo.png";
 
 function App() {
-  return <div className="App">Note Taking App</div>;
+  return (
+    <div className="app_div">
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Label" element={<Label />} />
+          <Route path="/Archive" element={<Archive />} />
+          <Route path="/Trash" element={<Trash />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home__container">
+      <div className="home__sidebar">
+        <Sidebar />
+      </div>
+      <div className="home__home">
+        <Link to="/RichTextEditor">
+          <button className="home__btn">Create New Note</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
